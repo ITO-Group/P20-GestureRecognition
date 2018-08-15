ad5f7305736890e87970e029e3bfff1ad27502f2
ZhouDavid <zjybox@126.com>
2018-08-15 03:07:15 -0700

    finish left-hand and right-hand gesture recognition

--- app/src/main/java/com/example/zhoujianyu/gesturerecognition/MainActivity.java
+++ app/src/main/java/com/example/zhoujianyu/gesturerecognition/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.zhoujianyu.gesturerecognition;
 import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.Touch;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,16 +17,29 @@ public class MainActivity extends AppCompatActivity {
     }
     public static final int ROW_NUM = 32;
     public static final int COL_NUM = 16;
-    public static final int TOUCH_THR = 500;
+    public static final int BOTTOM_TOUCH_THR = 80;
     public int screenWidth;
     public int screenHeight;
     public int capaWidth;
     public int capaHeight;
-    public int capacity_data[][] = new int[ROW_NUM][COL_NUM];
+    public int window=2;
+    public int capacity_data[][][] = new int[ROW_NUM][COL_NUM][window];
+    public int diff_data[][] = new int[ROW_NUM][COL_NUM];
+
+
+    //views
+//    TextView textView;
+    ImageView mKeyboardView;
+    ImageView mLandScapeKeyboardView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        textView = findViewById(R.id.sample_text);
+//        textView.setText("portrait");
+        mKeyboardView = findViewById(R.id.imageView);
+        mLandScapeKeyboardView = findViewById(R.id.imageView4);
+        mLandScapeKeyboardView.setVisibility(View.INVISIBLE);
         init();
         readDiffStart();
     }
@@ -36,29 +53,50 @@ public class MainActivity extends AppCompatActivity {
         capaHeight = screenHeight/ROW_NUM;
     }
     public void updateCapacity(short[] data){
+        // moving window
+        for(int w = 0;w+1<window;w++){
+            for(int i = 0;i<ROW_NUM;i++){
+                for(int j = 0;j<COL_NUM;j++) capacity_data[i][j][w] = capacity_data[i][j][w+1];
+            }
+        }
+        //update most current image
         for(int i = 0;i<ROW_NUM;i++){
-            for(int j = 0;j<COL_NUM;j++) capacity_data[i][j] = data[i*COL_NUM+j];
+            for(int j = 0;j<COL_NUM;j++){
+                capacity_data[i][j][window-1] = data[i*COL_NUM+j];
+            }
+        }
+        //calculate diff
+        for(int i = 0;i<ROW_NUM;i++){
+            for(int j = 0;j<COL_NUM;j++){
+                diff_data[i][j] = capacity_data[i][j][window-1]-capacity_data[i][j][window-2];
+            }
         }
     }
 
 
-    public int getGesture(){
-        boolean left_top = true; boolean right_top = true; boolean left_bottom = true; boolean right_bottom = true;
-        for(int i =4;i<7;i++){
-            if(capacity_data[i][0]<TOUCH_THR) {
-                left_top = false;
-            }
-            if(capacity_data[i][COL_NUM-1]<TOUCH_THR){
-                right_top = false;
+    public String getGesture(){
+        /** bottom_left_area:col_2-col_5
+         *  bottom_right_area: col_10-col_13
+         */
+        boolean bottom_left = false; boolean bottom_right = false;
+        for(int j = 2;j<=5;j++){
+            if(diff_data[ROW_NUM-1][j]>BOTTOM_TOUCH_THR) {
+                bottom_left = true;
+                break;
             }
         }
-        for(int i = ROW_NUM-6;i<ROW_NUM-3;i++){
-            if(capacity_data[i][0]<TOUCH_THR) left_bottom=false;
-            if(capacity_data[i][COL_NUM-1]<TOUCH_THR) right_bottom = false;
+        for(int j = 10;j<=13;j++){
+            if(diff_data[ROW_NUM-1][j]>BOTTOM_TOUCH_THR){
+                bottom_right = true;
+                break;
+            }
         }
-        if(left_top&&right_top&&left_bottom&&right_bottom) return 0;
-        else return -1;
-
+        if(bottom_left && !bottom_right) return "right_hand";
+        if(!bottom_left && bottom_right) return "left_hand";
+        if(bottom_left && bottom_right)  return "game";
+        else{
+            return "camera";
+        }
     }
     /**
      * callback method after everytime native_lib.cpp read an image of capacity data
@@ -67,7 +105,37 @@ public class MainActivity extends AppCompatActivity {
      */
     public void processDiff(short[] data) throws InterruptedException{
         updateCapacity(data);
-        int gesType = getGesture();
+        String gesType = getGesture();
+        if(gesType.equals("right_hand")) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    mLandScapeKeyboardView.setVisibility(View.INVISIBLE);
+                    mKeyboardView.setVisibility(View.VISIBLE);
+                    mKeyboardView.setImageResource(R.drawable.right);
+                }
+            });
+        }
+        else if(gesType.equals("left_hand")){
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    mLandScapeKeyboardView.setVisibility(View.INVISIBLE);
+                    mKeyboardView.setVisibility(View.VISIBLE);
+                    mKeyboardView.setImageResource(R.drawable.left);
+                }
+            });
+        }
+        else if(gesType.equals("game")){
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    mLandScapeKeyboardView.setVisibility(View.VISIBLE);
+                    mKeyboardView.setVisibility(View.INVISIBLE);
+                }
+            });
+        }
+
     }
 
     /**
